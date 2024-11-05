af28bad39b2c3581e8ebcbb96a98a8f918b28e0f
Boyinapalli Phani Shankar <113121127+16PHANI@users.noreply.github.com>
2024-11-06 04:40:53 +0530

    Add files via upload

---
+++ Char_Stuffing.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+int main() 
+{
+    int i, n;
+    char d[100];
+    printf("Enter the number of characters: ");
+    scanf("%d", &n);
+    printf("Enter the characters: ");
+    scanf("%s", d);
+    printf("The original data: %s\n", d);
+    printf("The transmitted data: dlestx");
+    for (i = 0; i < n; i++) 
+    {
+        if (d[i] == 'd' && d[i+1] == 'l' && d[i+2] == 'e') 
+        {
+            printf("dle");  
+        }
+        printf("%c", d[i]);
+    }
+    printf("dleetx\n");
+    printf("The received data: %s\n", d);
+    return 0;
+}
+
+
+
