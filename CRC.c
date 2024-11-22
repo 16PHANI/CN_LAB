c97116b1ed932b1eeaa272dbffa27fc952cfb7db
Boyinapalli Phani Shankar <113121127+16PHANI@users.noreply.github.com>
2024-11-22 22:54:43 +0530

    CRC.c

--- CRC.c
+++ CRC.c
@@ -1,85 +1,66 @@
-#include<stdio.h>
-#include<string.h>
-void crc(char msg[], char key[], char temp[], int msg_len, int key_len) 
+#include <stdio.h>
+#include <string.h>
+void computeCRC(char msg[], char key[], char remainder[], int msg_len, int key_len) 
 {
-    int i, x = key_len;
-    char temp1[10];
-    while (x <= msg_len) 
+    for (int i = 0; i <= msg_len - key_len; i++) 
     {
-        if (temp[0] == '1') 
-        {
-            for (i = 0; i < key_len; i++) 
+        if (remainder[0] == '1') 
+        { 
+            for (int j = 0; j < key_len; j++) 
             {
-                temp[i] = (temp[i] == key[i]) ? '0' : '1'; 
+                remainder[j] = (remainder[j] == key[j]) ? '0' : '1'; 
             }
         }
-        for (i = 0; i < key_len - 1; i++) 
-        {
-            temp1[i] = temp[i + 1];
-        }
-        temp1[key_len - 1] = msg[x]; 
-        temp1[key_len] = '\0';       
-
-        strcpy(temp, temp1); 
-        x++;
+        memmove(remainder, remainder + 1, key_len - 1);
+        remainder[key_len - 1] = msg[i + key_len];
     }
 }
 int main() 
 {
-    char key[10], msg[30], temp[10]; 
-    int i, msg_len, key_len;
+    char key[20], msg[100], remainder[20];
+    int msg_len, key_len;
+    // Sender Side
     printf("\nSender Side\n");
     printf("Enter message: ");
     scanf("%s", msg);
-    printf("Enter key: ");
+    printf("Enter key (polynomial): ");
     scanf("%s", key);
     msg_len = strlen(msg);
     key_len = strlen(key);
-    for (i = 0; i < key_len - 1; i++)
+    for (int i = 0; i < key_len - 1; i++) 
     {
         msg[msg_len + i] = '0';
     }
     msg[msg_len + key_len - 1] = '\0';
-    for (i = 0; i < key_len; i++) 
-    {
-        temp[i] = msg[i];
-    }
-    temp[key_len] = '\0';
-    crc(msg, key, temp, strlen(msg), key_len);
-    printf("CRC remainder (sender): %s\n", temp);
-    for (i = 0; i < key_len - 1; i++)
-    {
-        msg[msg_len + i] = temp[i];
-    }
+    strncpy(remainder, msg, key_len);
+    remainder[key_len] = '\0';
+    computeCRC(msg, key, remainder, strlen(msg), key_len);
+    printf("CRC remainder: %s\n", remainder);
+    strncpy(msg + msg_len, remainder, key_len - 1);
     msg[msg_len + key_len - 1] = '\0';
-    printf("Message with CRC (transmitted): %s\n", msg);
+    printf("Transmitted message with CRC: %s\n", msg);
     printf("\nReceiver Side\n");
     printf("Enter received message: ");
-    scanf("%s", msg);  
-    msg_len = strlen(msg); 
-    for (i = 0; i < key_len; i++)
+    scanf("%s", msg);
+    strncpy(remainder, msg, key_len);
+    remainder[key_len] = '\0';
+    computeCRC(msg, key, remainder, strlen(msg), key_len);
+    int isError = 0;
+    for (int i = 0; i < key_len - 1; i++) 
     {
-        temp[i] = msg[i];
-    }
-    temp[key_len] = '\0';
-    crc(msg, key, temp, msg_len, key_len);
-    int error = 0;
-    for (i = 0; i < key_len - 1; i++)
-    {
-        if (temp[i] != '0')
+        if (remainder[i] != '0') 
         {
-            error = 1;
+            isError = 1;
             break;
         }
     }
-    if (error) 
+    if (isError) 
     {
         printf("Error detected in received message.\n");
     } 
-    else
+    else 
     {
         printf("No error detected in received message.\n");
     }
     return 0;
 }
-
