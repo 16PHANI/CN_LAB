95cf4530ffa79a91d911f381a88c1697cca6dde0
Boyinapalli Phani Shankar <113121127+16PHANI@users.noreply.github.com>
2024-11-22 23:13:25 +0530

    Error_Detecting.c

--- Error_Detecting.c
+++ Error_Detecting.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-void addParityBit(char data[], char parityData[]) 
+int main() 
 {
-    int i, count = 0;
-    int n = strlen(data);
-    for (i = 0; i < n; i++) 
+    char data[100], parityData[101];
+    int count = 0;
+    int n;
+    printf("Enter binary data: ");
+    scanf("%s", data);
+    n = strlen(data);
+    for (int i = 0; i < n; i++) 
     {
         parityData[i] = data[i];
         if (data[i] == '1') 
@@ -12,22 +16,8 @@ void addParityBit(char data[], char parityData[])
             count++;
         }
     }
-    if (count % 2 == 0) 
-    {
-        parityData[n] = '0';
-    } 
-    else 
-    {
-        parityData[n] = '1'; 
-    }
-    parityData[n + 1] = '\0'; 
-}
-int main() 
-{
-    char data[100], parityData[101];
-    printf("Enter binary data: ");
-    scanf("%s", data);
-    addParityBit(data, parityData);
+    parityData[n] = (count % 2 == 0) ? '0' : '1';
+    parityData[n + 1] = '\0';
     printf("Data with parity bit: %s\n", parityData);
     return 0;
 }
