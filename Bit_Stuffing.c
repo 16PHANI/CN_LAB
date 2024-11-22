f74a0563bc699e7c0c5777249766bf6fe2d0b7e9
Boyinapalli Phani Shankar <113121127+16PHANI@users.noreply.github.com>
2024-11-22 23:10:55 +0530

    Bit_Stuffing.c

--- Bit_Stuffing.c
+++ Bit_Stuffing.c
@@ -1,44 +1,36 @@
 #include <stdio.h>
 int main() 
-{ 
-    int a[20], b[40], i, j = 0, k, count = 1, n;  
-    printf("Enter the frame size: "); 
-    scanf("%d", &n); 
-    printf("Enter the frame in the form of 0's and 1's: "); 
-    for (i = 0; i < n; i++) 
-    { 
-        scanf("%d", &a[i]); 
-    } 
-    i = 0; 
-    while (i < n) 
-    { 
+{
+    int a[20], b[40], n, count = 0, j = 0;
+    printf("Enter the frame size: ");
+    scanf("%d", &n);
+    printf("Enter the frame in the form of 0's and 1's: ");
+    for (int i = 0; i < n; i++) 
+    {
+        scanf("%d", &a[i]);
+    }
+    for (int i = 0; i < n; i++) 
+    {
+        b[j++] = a[i]; 
         if (a[i] == 1) 
-        { 
-            b[j++] = a[i]; 
-            count = 1; 
-            for (k = i + 1; k < n && a[k] == 1 && count < 5; k++) 
-            { 
-                b[j++] = a[k]; 
-                count++; 
-            } 
+        {
+            count++;
             if (count == 5) 
-            { 
+            {
                 b[j++] = 0; 
+                count = 0;
             }
-            i = k;
         } 
         else 
-        { 
-            b[j++] = a[i]; 
-            i++;
+        {
+            count = 0; 
         }
-    }  
-    printf("After bit stuffing: "); 
-    for (i = 0; i < j; i++) 
+    }
+    printf("After bit stuffing: ");
+    for (int i = 0; i < j; i++) 
     {
-        printf("%d", b[i]); 
-    } 
-    printf("\n"); 
-    
+        printf("%d", b[i]);
+    }
+    printf("\n");
     return 0;
 }
